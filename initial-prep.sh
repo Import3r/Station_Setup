8311a6086f39b5e9ec6e5a01cd1a1c6541e6c3d5
sigma <Import3r@users.noreply.github.com>
2021-02-22 04:12:23 +0200

    Fix progress messages newline escaping

--- initial-prep.sh
+++ initial-prep.sh
@@ -39,19 +39,19 @@ sudo add-apt-repository "deb https://repo.vivaldi.com/archive/deb/ stable main"
 sudo apt update -y
 
 # perform installation of desired software 
-echo "\n# Installing atom...\n"
+echo -e "\n# Installing atom...\n"
 sudo xargs -r -a packages.txt apt-get install -y
 wget "https://atom.io/download/deb" -O /tmp/DROPZONE/atom.deb && sudo apt install /tmp/DROPZONE/atom.deb -y && atom  # Atom Text Editor
-echo "\n# Installing Discord...\n"
+echo -e "\n# Installing Discord...\n"
 wget "https://discord.com/api/download?platform=linux&format=deb" -O /tmp/DROPZONE/discord.deb && sudo apt install /tmp/DROPZONE/discord.deb -y  # Discord
-echo "\n# Installing Burpsuite...\n"
+echo -e "\n# Installing Burpsuite...\n"
 wget "https://portswigger.net/burp/releases/download?product=community&version=2020.12.1&type=Linux" -O /tmp/DROPZONE/burpsuite.sh && chmod 755 /tmp/DROPZONE/burpsuite.sh && sudo /tmp/DROPZONE/burpsuite.sh  # BurpSuite
-echo "\n# Installing Bitwarden...\n"
+echo -e "\n# Installing Bitwarden...\n"
 wget "https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb" -O /tmp/DROPZONE/bitwarden.deb && sudo apt install /tmp/DROPZONE/bitwarden.deb  # Bitwarden
-echo "\n# Installing Docker...\n"
+echo -e "\n# Installing Docker...\n"
 sudo apt-get remove -y docker* && sudo apt install docker.io -y && sudo systemctl start docker && sudo systemctl enable docker  # Docker
 
-echo "\n# Installing Vivaldi browser...\n"
+echo -e "\n# Installing Vivaldi browser...\n"
 # install vivaldi browser
 sudo apt install vivaldi-stable -y
 tar xvzf ${base_dir}My-Vivaldi-settings.tar.gz -C ~
